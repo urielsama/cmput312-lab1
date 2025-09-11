010de5ec581383cac974d6edfa40559c70cbb7a6
Mahd Afzal <afzalmahd@gmail.com>
2025-09-11 00:23:51 -0600

    final position and orientation for q4

--- src/p_controller/p_controller.py
+++ src/p_controller/p_controller.py
@@ -5,6 +5,13 @@ from time import sleep
 
 def main(command_array):
     tank_drive = MoveTank(OUTPUT_A, OUTPUT_D)
+    left_motor = LargeMotor(OUTPUT_A)
+    right_motor = LargeMotor(OUTPUT_D)
+    left_motor.reset()
+    right_motor.reset()
+
+    gy = GyroSensor(INPUT_2)
+    gy.reset()  # reset gyro to 0
 
     for command in command_array:
         left_speed = command[0]
@@ -17,7 +24,13 @@ def main(command_array):
         tank_drive.on_for_seconds(
             SpeedPercent(left_speed), SpeedPercent(right_speed), duration
         )
+    
+    # Print final encoder positions
+    print(f"Left motor encoder position: {left_motor.position}")
+    print(f"Right motor encoder position: {right_motor.position}")
 
+    # Print the orientation of the robot using the gyro sensor
+    print(f"Final gyro angle: {gy.angle}")
 
 if __name__ == "__main__":
     main(paths.command_circle())
